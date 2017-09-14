9a086c8516a951989f4ee0928ecc7a1ba7973693
JOB <getabujob@gmail.com>
2017-09-14 20:00:39 +0300

    Using EF with Sql compact edition of database

---
+++ StudentCompact/FrmView.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace StudentCompact
+{
+    partial class FrmView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.metroGrid1 = new MetroFramework.Controls.MetroGrid();
+            this.metroBtnExit = new MetroFramework.Controls.MetroButton();
+            this.metroBtnDel = new MetroFramework.Controls.MetroButton();
+            this.metroBtnEdit = new MetroFramework.Controls.MetroButton();
+            this.metroBtnAdd = new MetroFramework.Controls.MetroButton();
+            this.metroPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.metroGrid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.Controls.Add(this.metroBtnAdd);
+            this.metroPanel1.Controls.Add(this.metroBtnEdit);
+            this.metroPanel1.Controls.Add(this.metroBtnDel);
+            this.metroPanel1.Controls.Add(this.metroBtnExit);
+            this.metroPanel1.Controls.Add(this.metroGrid1);
+            this.metroPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(20, 60);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(581, 378);
+            this.metroPanel1.Style = MetroFramework.MetroColorStyle.Magenta;
+            this.metroPanel1.TabIndex = 0;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            // 
+            // metroGrid1
+            // 
+            this.metroGrid1.AllowUserToResizeRows = false;
+            this.metroGrid1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.metroGrid1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.metroGrid1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.metroGrid1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.metroGrid1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.metroGrid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.metroGrid1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.metroGrid1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.metroGrid1.EnableHeadersVisualStyles = false;
+            this.metroGrid1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.metroGrid1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.metroGrid1.Location = new System.Drawing.Point(0, 0);
+            this.metroGrid1.Name = "metroGrid1";
+            this.metroGrid1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.metroGrid1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.metroGrid1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.metroGrid1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.metroGrid1.Size = new System.Drawing.Size(581, 329);
+            this.metroGrid1.TabIndex = 2;
+            // 
+            // metroBtnExit
+            // 
+            this.metroBtnExit.Location = new System.Drawing.Point(489, 335);
+            this.metroBtnExit.Name = "metroBtnExit";
+            this.metroBtnExit.Size = new System.Drawing.Size(75, 23);
+            this.metroBtnExit.TabIndex = 3;
+            this.metroBtnExit.Text = "&Exit";
+            this.metroBtnExit.UseSelectable = true;
+            // 
+            // metroBtnDel
+            // 
+            this.metroBtnDel.Location = new System.Drawing.Point(408, 335);
+            this.metroBtnDel.Name = "metroBtnDel";
+            this.metroBtnDel.Size = new System.Drawing.Size(75, 23);
+            this.metroBtnDel.TabIndex = 2;
+            this.metroBtnDel.Text = "&Del";
+            this.metroBtnDel.UseSelectable = true;
+            // 
+            // metroBtnEdit
+            // 
+            this.metroBtnEdit.Location = new System.Drawing.Point(327, 335);
+            this.metroBtnEdit.Name = "metroBtnEdit";
+            this.metroBtnEdit.Size = new System.Drawing.Size(75, 23);
+            this.metroBtnEdit.TabIndex = 0;
+            this.metroBtnEdit.Text = "Edit";
+            this.metroBtnEdit.UseSelectable = true;
+            // 
+            // metroBtnAdd
+            // 
+            this.metroBtnAdd.Location = new System.Drawing.Point(246, 335);
+            this.metroBtnAdd.Name = "metroBtnAdd";
+            this.metroBtnAdd.Size = new System.Drawing.Size(75, 23);
+            this.metroBtnAdd.TabIndex = 1;
+            this.metroBtnAdd.Text = "Add";
+            this.metroBtnAdd.UseSelectable = true;
+            // 
+            // FrmView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 458);
+            this.Controls.Add(this.metroPanel1);
+            this.Name = "FrmView";
+            this.Text = "Students";
+            this.metroPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.metroGrid1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private MetroFramework.Controls.MetroButton metroBtnAdd;
+        private MetroFramework.Controls.MetroButton metroBtnEdit;
+        private MetroFramework.Controls.MetroButton metroBtnDel;
+        private MetroFramework.Controls.MetroButton metroBtnExit;
+        private MetroFramework.Controls.MetroGrid metroGrid1;
+    }
+}
+
---
+++ StudentCompact/FrmView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlServerCe;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentCompact
+{
+    public partial class FrmView : MetroFramework.Forms.MetroForm
+    {
+
+        private const string dbFileName = @"C:\Users\JOB\Desktop\Projects\school finance\Database prototypes\StudentCompact\Student.sdf";
+
+        private static string dbConnectionString =
+           string.Format("Data Source=|DataDirectory|{0};Max Database Size=4091", dbFileName);
+        public FrmView()
+        {
+            InitializeComponent();
+
+            using (var connection = new SqlCeConnection(dbConnectionString))
+            {
+                using (var context = new StudentContext(connection))
+                {
+                    //test works
+                    // var studentList= context.Students.ToList();
+                   
+                }
+            }
+        }
+    }
+}
